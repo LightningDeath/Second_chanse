3b1197c5747447f940a9954cc5bbb8c36ce86cab
Василий Андреевич Дударевич <94409566+LightningDeath@users.noreply.github.com>
2023-03-19 20:18:03 +0300

    Add files via upload

--- Task_3.py
+++ Task_3.py
@@ -1,2 +1,13 @@
-a = int(input('Введите целое положительное число n:'))
-print(f'сумма чисел n+nn+nnn:{a}{a+a}{a*3}')
+def user_data(name, last_n, date, city, email, phone):
+    print(f'{name} {last_n} {date} года рождения, проживает в городе {city}, email: {email}, телефон: {phone}')
+    return
+
+
+n = input('Введите имя пользователя: ')
+l = input('Введите фамилию пользователя: ')
+d = input('Введите год рождения пользователя: ')
+c = input('Введите город рождения пользователя: ')
+e = input('Введите email пользователя: ')
+p = input('Введите телефон пользователя: ')
+
+user_data(name=n, last_n=l, date=d, city=c, email=e, phone=p)
